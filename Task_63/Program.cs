6ad09e8ac823a79a5a72ef5e21d4682de7cad7c5
George_M <msgoha555@yandex.ru>
2022-06-19 00:26:34 +0300

    Completed task 63

--- Task_63/Program.cs
+++ Task_63/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая выводит все натуральные числа в промежутке от 1 до заданного числа (N)
+
+Console.Write("Задайте число: ");
+int n = int.Parse(Console.ReadLine());
+
+void NaturalNumber(int num)
+{
+    if (num == 1) return;
+    num--;
+    NaturalNumber(num);
+    Console.Write($"{num} ");
+}
+
+NaturalNumber(n + 1);
