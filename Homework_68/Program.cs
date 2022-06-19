515b95f6e285cce66e1172e968b5ed70b950d7a1
George_M <msgoha555@yandex.ru>
2022-06-19 14:47:52 +0300

    Completed Homework 68

---
+++ Homework_68/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.Write("Задайте число неотрицательное m = ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Задайте число неотрицательное n = ");
+int num2 = int.Parse(Console.ReadLine());
+
+int AckermanMethod(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return AckermanMethod(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return AckermanMethod(m - 1, AckermanMethod(m, n - 1));
+    }
+    return 0;
+}
+
+int result = AckermanMethod(num1, num2);
+Console.Write($"A({num1}, {num2}) => {result}");
