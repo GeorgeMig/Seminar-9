b39598b99a6c0e5b1a962e3f428345d408337b14
George_M <msgoha555@yandex.ru>
2022-06-19 12:11:19 +0300

    Completed Homework 64

---
+++ Homework_64/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа, которая выводит все натуральные числа в промежутке от N до 1 при помощи рекурсии;
+
+Console.Write("Задайте число: ");
+int n = int.Parse(Console.ReadLine());
+
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num}, ");
+    NaturalNumber(num - 1);
+
+}
+
+NaturalNumber(n);
