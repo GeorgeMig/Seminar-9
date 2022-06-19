95afbe51465000ab43f896dc270c3ae28c253e8b
George_M <msgoha555@yandex.ru>
2022-06-19 14:20:01 +0300

    Completed Homework 66

---
+++ Homework_66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа, которая находит сумму натуральных чисел в заданном промежутке. 
+
+Console.Write("Задайте число начало= ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Задайте число конец = ");
+int n = int.Parse(Console.ReadLine());
+int summer = 0;
+
+
+
+
+void SumNaturalNumber(int start, int end, int sum) // метод подсчета суммы натуральных чисел в заданном промежутке.
+{
+    if (start <= end)
+    {
+        sum += start;
+        start++;
+        NaturalNumber(start, end, sum);
+    }
+    else
+    {
+        Console.Write(sum);
+    }
+
+
+}
+
+SumNaturalNumber(m, n, summer); // выполнение метода
