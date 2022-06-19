c96895f4ab38d59ddd00ae8a71b8901cee0b78a1
George_M <msgoha555@yandex.ru>
2022-06-19 11:04:30 +0300

    Completed Nask 67

---
+++ Task_67/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая принимает на вход число и возвращает сумму его цифр
+
+Console.Write("Задайте число n = ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+void SumNumber(int count, int summ) // метод нахождения суммы цифр в заданном числе при помощи рекурсии
+{
+    if (count == 0) 
+    {
+        Console.Write($"Сумма цифр в числе {n} равна {summ}"); 
+        return;
+    }
+    int number = count % 10;
+    count /= 10;
+    summ += number;
+    SumNumber(count, summ);
+
+}
+
+
+SumNumber(n, sum); // выполнение метода
